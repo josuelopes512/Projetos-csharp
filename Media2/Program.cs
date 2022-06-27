83e191ad4332bd88c81d12f2a29094e6eb61d290
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-06-26 23:32:39 -0300

    media ponderada 3 valores

---
+++ Media2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace Media2
+{
+    class Program
+    {
+        static void Main(string[] args){
+            double a, b, c, media, p1, p2, p3;
+            string result;
+
+            p1 = 2;
+            p2 = 3;
+            p3 = 5;
+
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            media = ((a * p1) + (b * p2)+ (c * p3))/(p1+p2+p3);
+            result = media.ToString("F1", CultureInfo.InvariantCulture);
+            
+            Console.WriteLine($"MEDIA = {result}");
+        }
+    }
+}
