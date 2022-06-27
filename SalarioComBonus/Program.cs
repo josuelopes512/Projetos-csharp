e2e314690cb5c1d0c7b6bcc2f1ed807996afc315
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-06-27 11:03:28 -0300

    atividade Salario com bonus

---
+++ SalarioComBonus/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalarioComBonus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result, nome;
+            double a, b;
+            
+            nome = Console.ReadLine();
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            result = (a + (b*15)/100).ToString("F2", CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"TOTAL = R$ {result}");
+        }
+    }
+}
