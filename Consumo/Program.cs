04b0e73b2dc613f82bb78d52894d6a913d2b3ec4
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-06-27 15:17:39 -0300

    atividade Consumo

---
+++ Consumo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Consumo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, total;
+            string result;
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            total = a/b;
+            
+            result = total.ToString("F3", CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"{result} km/l");
+
+
+        }
+    }
+}
