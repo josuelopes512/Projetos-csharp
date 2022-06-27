fcba03757113893f739539c9842d93a5b09e031d
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-06-27 19:36:56 -0300

    atividade GastoCombustivel

---
+++ GastoCombustivel/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace GastoCombustivel
+{
+    public class Program
+    {
+        static void Main(string[] args){
+            int tempo, velocidade;
+            double media, km;
+            string result;
+
+            tempo = int.Parse(Console.ReadLine());
+            velocidade = int.Parse(Console.ReadLine());
+
+            km = tempo * velocidade;
+            media = km/12;
+
+            result = media.ToString("F3", CultureInfo.InvariantCulture);
+            Console.WriteLine($"{result}");
+        }
+    }
+}
