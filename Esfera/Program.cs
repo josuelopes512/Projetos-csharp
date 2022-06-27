d3fbdd315b76c38f3d421d779e3bf11f945b286c
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-06-27 14:27:45 -0300

    calculo esfera

---
+++ Esfera/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Esfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, volume;
+            string result;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            volume = (4.0/3.0) * 3.14159 * Math.Pow(raio, 3);
+
+            result = volume.ToString("F3", CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"VOLUME = {result}");
+        }
+    }
+}
