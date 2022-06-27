9c91398066b2e7185340d53c6b5bcd61574232a3
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-06-27 10:40:02 -0300

    Salario activity

---
+++ Salario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double c; 
+            string result;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            result = (b*c).ToString("F2", CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"NUMBER = {a}");
+            Console.WriteLine($"SALARY = U$ {result}");
+        }
+    }
+}
