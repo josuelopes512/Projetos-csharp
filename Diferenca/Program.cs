852ff7ffbeeaf2820f36264aec57a115eb1428e5
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-06-26 23:47:21 -0300

    calcula diferenca

---
+++ Diferenca/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Diferenca
+{
+    class Program
+    {
+        static void Main(string[] args){
+            int a, b, c, d, diferenca;
+            string result;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+            
+            diferenca = ((a*b) - (c*d));
+            result = diferenca.ToString();
+            
+            Console.WriteLine($"DIFERENCA = {result}");
+        }
+    }
+}
