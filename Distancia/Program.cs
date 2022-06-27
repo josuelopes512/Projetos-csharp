cd6c1d6cf749d0bdc451c525c03fd8eed6c6fb4e
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-06-27 19:23:04 -0300

    atividade distancia

---
+++ Distancia/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Distancia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min;
+            string result;
+            min = (60 * int.Parse(Console.ReadLine())) / 30;
+            result = min.ToString();
+            Console.WriteLine($"{result} minutos");
+        }
+    }
+}
