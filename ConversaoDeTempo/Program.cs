7e1483f53dc93736f1e25e4faf3a0592f888a320
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-06-28 09:25:43 -0300

    Atividade Conversão de Tempo

---
+++ ConversaoDeTempo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversaoDeTempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, h, m, s;
+
+            n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            h = Math.Floor(n/3600);
+            m = Math.Floor((n - (h*3600))/60);
+            s = Math.Floor(n%60);
+
+            Console.WriteLine($"{h}:{m}:{s}");
+        }
+    }
+}
