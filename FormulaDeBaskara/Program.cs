a6c8e84d85b6f9d619037e696cb88d1f19c5d9d1
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-06-30 16:29:15 -0300

    refact baskara

---
+++ FormulaDeBaskara/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FormulaDeBaskara
+{
+    class Program
+    {
+        static int[] StringToIntVec(string[] value){
+            int[] new_value = new int[value.Length];
+            for (int i = 0; i < new_value.Length; i++)
+            {
+                new_value[i] = int.Parse(value[i]);
+            }
+            return new_value;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] input;
+            input = StringToIntVec(Console.ReadLine().Split(' '));
+        }
+    }
+}
