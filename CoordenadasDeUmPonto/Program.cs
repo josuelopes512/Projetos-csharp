5efecc9f7619d598a131368731d7cfdbd0861f4b
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-01 14:30:11 -0300

    add atividade Coordenadas de um Ponto

---
+++ CoordenadasDeUmPonto/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace CoordenadasDeUmPonto
+{
+    class Program
+    {
+        static double[] StringToDoubleVec(string[] value){
+            double[] new_value = new double[value.Length];
+            for (int i = 0; i < new_value.Length; i++)
+            {
+                try
+                {
+                    new_value[i] = double.Parse(value[i], CultureInfo.InvariantCulture);
+                }
+                catch (System.Exception)
+                {
+                    Console.WriteLine("ERROR");
+                }
+            }
+            return new_value;
+        }
+
+        static void Main(string[] args)
+        {
+            double[] valores = StringToDoubleVec(Console.ReadLine().Split(' '));
+            double x, y;
+
+            x = valores[0];
+            y = valores[1];
+
+
+            if(x == 0.0 && y == 0.0){
+                Console.WriteLine("Origem");
+            }
+            else if(x != 0.0 && y == 0.0){
+                Console.WriteLine("Eixo X");
+            }
+            else if(x == 0.0 && y != 0.0){
+                Console.WriteLine("Eixo Y");
+            }
+            else if(x > 0.0){
+                if(y > 0.0) Console.WriteLine("Q1");
+                else Console.WriteLine("Q4");
+            }
+            else if(x < 0.0){
+                if(y > 0.0) Console.WriteLine("Q2");
+                else Console.WriteLine("Q3");
+            }
+        }
+    }
+}
