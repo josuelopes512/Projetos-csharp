2433d772ac6bcbd04110a0b1c309237ac1f8f7ff
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-02 19:38:54 -0300

    add atividade Pares, Ímpares, Positivos e Negativos

---
+++ ParesImparesPositivosENegativos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ParesEntreCincoNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int par=0, impar=0, pos=0, neg=0;
+            for (int i = 0; i < 5; i++)
+            {
+                int valor = int.Parse(Console.ReadLine());
+                if (valor % 2 == 0) par++;
+                else impar++;
+                if (valor > 0) pos++;
+                else if (valor == 0) continue;
+                else neg++;
+            }
+
+            Console.WriteLine($"{par.ToString()} valor(es) par(es)");
+            Console.WriteLine($"{impar.ToString()} valor(es) impar(es)");
+            Console.WriteLine($"{pos.ToString()} valor(es) positivo(s)");
+            Console.WriteLine($"{neg.ToString()} valor(es) negativo(s)");
+        }
+    }
+}
