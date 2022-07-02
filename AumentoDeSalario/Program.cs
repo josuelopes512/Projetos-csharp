df24d9ac362ea80880dd6f64a4040512b8b8f906
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-02 14:37:09 -0300

    add atividade Aumento de Salário

---
+++ AumentoDeSalario/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace AumentoDeSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double reajuste, value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            int percentual = 0;
+            if (value >= 0.00 && value <= 400.00) percentual = 15;
+            else if (value >= 400.01 && value <= 800.00) percentual = 12;
+            else if (value >= 800.01 && value <= 1200.00) percentual = 10;
+            else if (value >= 1200.01 && value <= 2000.00) percentual = 7;
+            else if (value > 2000.00) percentual = 4;
+
+            reajuste = (value * percentual)/100;
+            value += reajuste;
+            
+            Console.WriteLine($"Novo salario: {value.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Reajuste ganho: {reajuste.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Em percentual: {percentual.ToString()} %");
+        }
+    }
+}
