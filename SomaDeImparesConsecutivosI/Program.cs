d31b1eb27871992000fc17d4047fb487a76c96aa
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-02 20:13:12 -0300

    add atividade Soma de Impares Consecutivos I

---
+++ SomaDeImparesConsecutivosI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SomaDeImparesConsecutivosI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1, valor2, soma=0;
+            valor1 = int.Parse(Console.ReadLine());
+            valor2 = int.Parse(Console.ReadLine());
+
+            if (valor1 < valor2) {
+                for (int i = valor1 + 1; i < valor2; i++) {
+                    if (i % 2 != 0) {
+                        soma += i;
+                    }
+                }
+            } 
+            else {
+                for (int i= valor2 + 1; i < valor1; i++) {
+                    if (i % 2 != 0) {
+                        soma += i;
+                    }
+                }
+            }
+            
+            Console.WriteLine(soma);
+        }
+    }
+}
