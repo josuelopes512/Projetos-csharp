820fbb5f71053faaa3c2e7d7976c9f06f8daa601
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-02 14:56:15 -0300

    add atividade animal

---
+++ Animal/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = new string[3];
+            for (int i = 0; i < data.Length; i++){
+                data[i] =  Console.ReadLine();
+            }
+            if (data[0] == "vertebrado"){
+                if (data[1] == "ave")
+                {
+                    if (data[2] == "carnivoro")
+                    {
+                        Console.WriteLine("aguia");
+                    }
+                    else if (data[2] == "onivoro")
+                    {
+                        Console.WriteLine("pomba");
+                    }
+                }
+                else if (data[1] == "mamifero")
+                {
+                    if (data[2] == "onivoro")
+                    {
+                        Console.WriteLine("homem");
+                    }
+                    else if (data[2] == "herbivoro")
+                    {
+                        Console.WriteLine("vaca");
+                    }
+                }
+            }
+            else if (data[0] == "invertebrado"){
+                if (data[1] == "inseto")
+                {
+                    if (data[2] == "hematofago")
+                    {
+                        Console.WriteLine("pulga");
+                    }
+                    else if (data[2] == "herbivoro")
+                    {
+                        Console.WriteLine("lagarta");
+                    }
+                }
+                else if (data[1] == "anelideo")
+                {
+                   if (data[2] == "hematofago")
+                    {
+                        Console.WriteLine("sanguessuga");
+                    }
+                    else if (data[2] == "onivoro")
+                    {
+                        Console.WriteLine("minhoca");
+                    } 
+                }
+            }
+        }
+    }
+}
