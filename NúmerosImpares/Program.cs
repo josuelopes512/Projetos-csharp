f66255c99f8b13e40e4d4a948f69daede2a52d1c
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-02 19:42:56 -0300

    add atividade Números Ímpares

---
+++ NúmerosImpares/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NúmerosImpares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+            for (int i = 0; i < valor+1; i++){
+                if (!(i % 2 == 0)) Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
