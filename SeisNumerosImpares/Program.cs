0583dbb6bd5bb662cafe4a686ead09a2ca0913cc
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-02 20:06:17 -0300

    add atividade Seis Números Ímpares

---
+++ SeisNumerosImpares/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SeisNumerosImpares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count=0, valor = int.Parse(Console.ReadLine());
+            while (count < 6)
+            {
+                if (valor % 2 != 0)
+                {
+                    Console.WriteLine(valor);
+                    count++;
+                }
+                valor++;
+            }
+        }
+    }
+}
