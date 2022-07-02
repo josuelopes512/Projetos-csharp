043cd4d083e46e11272a1a582c50e71bfe66260a
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-02 19:12:12 -0300

    add atividade Quadrado de Pares

---
+++ QuadradoDePares/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace QuadradoDePares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+            if (valor >= 5 && valor <=2000)
+            {
+                for (int i = 1; i < valor+1; i++)
+                {
+                    if (i % 2 == 0) Console.WriteLine($"{i}^2 = {Math.Pow(i, 2)}");
+                    
+                }
+            }
+        }
+    }
+}
