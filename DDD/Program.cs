6676f234296973fda411c0856841e1833073ef60
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-02 15:04:55 -0300

    add atividade DDD

---
+++ DDD/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+            string estado;
+
+            if(valor == 61) estado = "Brasilia";
+            else if(valor == 71) estado = "Salvador";
+            else if(valor == 11) estado = "Sao Paulo";
+            else if(valor == 21) estado = "Rio de Janeiro";
+            else if(valor == 32) estado = "Juiz de Fora";
+            else if(valor == 19) estado = "Campinas";
+            else if(valor == 27) estado = "Vitoria";
+            else if(valor == 31) estado = "Belo Horizonte";
+            else estado = "DDD nao cadastrado";
+            Console.WriteLine($"{estado}");
+        }
+    }
+}
