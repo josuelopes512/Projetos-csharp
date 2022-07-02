891d619c600124c06d6e1142ffbaeb0123ac561d
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-02 20:25:42 -0300

    add atividade Positivos e Média

---
+++ PositivosEMedia/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace PositivosEMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pos=0;
+            double soma=0;
+            for (int i = 0; i < 6; i++)
+            {
+                double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (valor > 0){
+                    soma += valor; 
+                    pos++;
+                } 
+            }
+            Console.WriteLine($"{pos} valores positivos");
+            Console.WriteLine((soma/pos).ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
