3f1f2b5cbc9e8ae8986c0c3051b547791dbeea6f
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-02 18:34:49 -0300

    add atividade Números Positivos

---
+++ NumerosPositivos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace NumerosPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            
+            for (int i = 0; i < 6; i++)
+            {
+                double num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (num > 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"{count.ToString()} valores positivos");
+        }
+    }
+}
