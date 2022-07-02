d3dd2b9ac5faacb4429e7fbc04dc3b13c8c38cfb
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-02 12:12:41 -0300

    add atividade Triangulo

---
+++ Triangulo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Triangulo
+{
+    class Program
+    {
+        static double[] StringToDoubleVec(string[] value){
+            double[] new_value = new double[value.Length];
+            for (int i = 0; i < new_value.Length; i++)
+            {
+                new_value[i] = double.Parse(value[i], CultureInfo.InvariantCulture);
+            }
+            return new_value;
+        }
+
+        static void Main(string[] args)
+        {
+            double[] vetor = StringToDoubleVec(Console.ReadLine().Split(' '));
+            double a, b, c;
+            string result;
+            a = vetor[0]; b = vetor[1]; c = vetor[2];
+
+            if (a < b + c && b < a + c && c < a + b)
+            {
+                result = (a+b+c).ToString("F1", CultureInfo.InvariantCulture);
+                Console.WriteLine($"Perimetro = {result}");
+            }
+            else
+            {
+                result = ((c * (a + b))/2).ToString("F1", CultureInfo.InvariantCulture);
+                Console.WriteLine($"Area = {result}");
+            }
+        }
+    }
+}
