3732154cbe4868a293ffc1f5301ad23000174f4d
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-02 20:33:56 -0300

    add atividade Intervalo 2

---
+++ Intervalo2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Intervalo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vin=0, vout=0, qtd = int.Parse(Console.ReadLine());
+            for (int i = 0; i < qtd; i++){
+                int valor = int.Parse(Console.ReadLine());
+                if (valor >= 10 && valor <= 20) vin++;
+                else vout++;
+            }
+            Console.WriteLine($"{vin} in");
+            Console.WriteLine($"{vout} out");
+        }
+    }
+}
