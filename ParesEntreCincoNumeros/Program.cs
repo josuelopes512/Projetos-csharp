0d59456eec695bee00d0b7530c549cff981da7fd
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-02 19:20:07 -0300

    add atividade Pares entre Cinco Números

---
+++ ParesEntreCincoNumeros/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ParesEntreCincoNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                int valor = int.Parse(Console.ReadLine());
+                if (valor % 2 == 0) count++;
+            }
+
+            Console.WriteLine($"{count.ToString()} valores pares");
+        }
+    }
+}
