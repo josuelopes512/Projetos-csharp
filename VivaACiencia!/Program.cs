781fbf2de152a750d6f3ca3fa63c18c73a2cf4e5
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-04 11:54:20 -0300

    add atividade Viva a Ciencia!

---
+++ VivaACiencia!/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VivaACiencia_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = Console.ReadLine();
+            Console.WriteLine("Nao se refuta ciencia com opiniao");
+        }
+    }
+}
