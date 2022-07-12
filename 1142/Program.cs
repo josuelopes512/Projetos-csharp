aae7f4ddf18c12657b808bded54af79411a33ec5
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-12 09:04:02 -0300

    add atividade PUM

---
+++ 1142/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1142
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0, input = int.Parse(Console.ReadLine());
+            for (int i = 0; i < input; i++)
+            {
+                string data = "";
+                for (int j = 0; j < 4; j++)
+                {
+                    count++;
+                    if(j == 3)
+                    {
+                        data += "PUM";
+                    }
+                    else
+                    {
+                        data += $"{count} ";
+                    }
+                }
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
