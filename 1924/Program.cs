5e3d3d90db7b6fdd3ef218169111a4780e63ab41
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-12 20:52:30 -0300

    add atividade Vitória e a Indecisão

---
+++ 1924/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1924
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtd = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtd; i++)
+            {
+                string input = Console.ReadLine();
+            }
+            
+            Console.WriteLine("Ciencia da Computacao");
+        }
+    }
+}
