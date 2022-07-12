d890acfc19dafe0dee4fb45d246c67f98c78100c
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-12 09:16:53 -0300

    add atividade Sequências Crescentes

---
+++ 1146/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+
+namespace _1146
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList inputs = new ArrayList();
+            
+            while (true)
+            {
+                int data = int.Parse(Console.ReadLine());
+                if (data == 0) break;
+                inputs.Add(data);
+            }
+            foreach (int item in inputs)
+            {
+                string output = "";
+                for (int i = 0; i < item; i++)
+                {
+                    output += $"{i+1} ";
+                }
+                Console.WriteLine(output.Trim());
+            }
+        }
+    }
+}
