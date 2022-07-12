9fd39d3394aaf44c49995897b666a4e1e79711f2
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-12 09:25:16 -0300

    Add atividade Senha Fixa

---
+++ 1114/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+
+namespace _1114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            ArrayList inputs = new ArrayList();
+            
+            while (true)
+            {
+                int data = int.Parse(Console.ReadLine());
+                inputs.Add(data);
+                if (data == 2002) break; 
+            }
+            foreach (int item in inputs)
+            {
+                if (item == 2002)
+                {
+                    Console.WriteLine("Acesso Permitido");
+                }
+                else
+                {
+                    Console.WriteLine("Senha Invalida");
+                }
+            }
+        }
+    }
+}
