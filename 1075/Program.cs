c57325f7c1cfa01fd8313d3a800745afd2f2f585
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-13 20:27:51 -0300

    add atividade Resto 2

---
+++ 1075/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1075
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= 10000; i++) if (i % n == 2) Console.WriteLine(i);            
+        }
+    }
+}
