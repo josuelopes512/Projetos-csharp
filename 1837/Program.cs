698662cb5e4f6b06d9e806f125b8c811ab1fda00
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-13 15:43:56 -0300

    add atividade prefácio

---
+++ 1837/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1837
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            int q=0, r=0, a=int.Parse(inputs[0]), b=int.Parse(inputs[1]);
+            if (a >= 0)
+            {
+                q = a / b;
+                r = a % b;
+            }
+            else
+            {
+                int x = 0, y=0;
+                if (b < 0) x = b * -1;
+                else x = b;
+
+                for (r = 0; r < x; r++)
+                {
+                    y = a - r;
+                    if (y % b == 0) break;
+                }
+                q = y/b;
+            }
+            Console.WriteLine($"{q} {r}");
+
+        }
+    }
+}
