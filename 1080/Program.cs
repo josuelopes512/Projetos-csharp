3f4eacac045ed63f2dfd3f54ca558803d662eb03
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-13 20:01:47 -0300

    add atividade Maior e Posição

---
+++ 1080/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1080
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pos = 0, temp = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                int data = int.Parse(Console.ReadLine());
+                if (data > temp) 
+                {
+                    temp = data;
+                    pos = i;
+                }
+            }
+            Console.WriteLine($"{temp}");
+            Console.WriteLine($"{pos}");
+        }
+    }
+}
