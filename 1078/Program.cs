b1d5158eddc85173645d5b6674ef89652206c070
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-13 19:54:59 -0300

    add atividade Tabuada

---
+++ 1078/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1078
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < 10+1; i++) Console.WriteLine($"{i} x {n} = {i*n}");
+        }
+    }
+}
