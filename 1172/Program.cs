94fb1745b573ed4229d5cc8546266025ee495661
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-07-13 20:23:06 -0300

    add atividade Substituição em Vetor I

---
+++ 1172/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1172
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[10];
+            for (int i = 0; i < data.Length; i++) data[i] = int.Parse(Console.ReadLine());
+            for (int i = 0; i < data.Length; i++) if(data[i] <= 0) data[i] = 1;
+            for (int i = 0; i < data.Length; i++) Console.WriteLine($"X[{i}] = {data[i]}");
+        }
+    }
+}
