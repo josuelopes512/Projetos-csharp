1511bb7315945881c3a072b5eccba6997e1bc1ab
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-14 09:12:05 -0300

    add atividade Múltiplos de 13

---
+++ 1132/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1132
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma=0, v1 = int.Parse(Console.ReadLine()), v2 = int.Parse(Console.ReadLine());
+            
+            if (v1 < v2) {
+                for (int i = v1; i <= v2; i++) 
+                {
+                    if (i % 13 != 0) soma += i;
+                }
+            }
+            else {
+                for (int i = v2; i <= v1; i++) 
+                {
+                    if (i % 13 != 0) 
+                    {
+                        soma += i;
+                    }
+                }
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
