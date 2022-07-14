206cffe1e8d9c9d8e648ff6405308ca40303b90e
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-14 09:32:32 -0300

    add atividade Troca em Vetor I

---
+++ 1175/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1175
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] entrada = new int[20];
+            int temp = 0;
+            for (int i = 0; i < entrada.Length; i++)
+            {
+                entrada[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i=0, j=(entrada.Length-1); i<(entrada.Length/2); i++, j--)
+            {
+                temp=entrada[i];
+                entrada[i]=entrada[j];
+                entrada[j]=temp;
+            }
+
+            for (int i = 0; i < entrada.Length; i++)
+            {
+                Console.WriteLine($"N[{i}] = {entrada[i]}");
+            }
+            
+        }
+    }
+}
