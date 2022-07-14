3fa73e1c8a66bfcf4f0054e004aaad10610f3a27
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-14 08:37:00 -0300

    add atividade Preenchimento de Vetor I

---
+++ 1173/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1173
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor =  new int[10];
+            vetor[0] = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 10; i++) vetor[i] = vetor[i-1] * 2;
+            for (int i=0; i <10; i++) Console.WriteLine($"N[{i}] = {vetor[i]}");
+        }
+    }
+}
