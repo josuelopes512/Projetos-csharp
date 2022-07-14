0cbc53870f6d3336e0ce0c8d675c89595905c49f
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-14 10:04:01 -0300

    add atividade Acima da Diagonal Principal

---
+++ 1183/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1183
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0.0;
+            double[,] M = new double[12, 12];
+            string T = Console.ReadLine().Split(' ')[0];
+            int p=1;             
+            for(int x=0;x<12;x++)
+            {
+                for(int y=0; y<12; y++)
+                {
+                    M[x, y] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
+            }
+            
+            for(int z=0; z<12;z++)
+            {
+                for(int c=p; c<12;c++)
+                    a+=M[z, c];
+                p++;
+            }
+
+            // if(T=="S"){}
+            if(T=="M") a=a/66.0;
+            
+            Console.WriteLine($"{a.ToString("F1", CultureInfo.InvariantCulture)}");
+            
+        }
+    }
+}
