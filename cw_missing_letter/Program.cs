59493f0a6c0417989e6b6d22529f4a8daeefafe3
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-15 16:04:02 -0300

    add para revisar

---
+++ cw_missing_letter/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+namespace cw_missing_letter
+{
+    public class Kata
+    {
+        public static char FindMissingLetter(char[] array)
+        {
+
+            var dsdf = String.Join(",", array).ToLower().Split(',');
+            foreach (var item in dsdf)
+            {
+                Console.WriteLine(item);
+            }
+            return " ".ToChar();
+//             int indice_j= 0;
+//             char[] alfabeto = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+//             for (int i = 0; i < array.Length; i++)
+//             {
+//                 // Console.WriteLine($"{indice_j}");
+//                 if (indice_j == 0)
+//                 {
+//                     for (int j = 0; j < alfabeto.Length; j++)
+//                     {
+//                         if (Char.ToLower(array[i]) != alfabeto[j]) continue;
+//                         else
+//                         {
+//                             indice_j = j;
+//                             break;
+//                         }
+//                     }
+//                 }
+// //                 Console.WriteLine($"{array[i]} -- {alfabeto[indice_j]} --- {indice_j}");
+//                 if((Char.ToLower(array[i]) != alfabeto[indice_j])){
+//                     return alfabeto[indice_j];
+//                     // Console.WriteLine($"{array[i]} -- {alfabeto[indice_j]} --- {indice_j}");
+//                     // break;
+//                 }
+//                 indice_j++;
+                
+//                 // Console.WriteLine($"{array[i]} -- {indice_j}");
+//                 // Console.WriteLine($"{array[i]} -- {alfabeto[indice_j++]} -- {indice_j}");
+//             }
+//             // foreach (var item in alfabeto)
+//             // {
+//             //     Console.WriteLine(item);
+//             // }
+//             return ' ';
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lsdk = Kata.FindMissingLetter(new [] { 'O','Q','R','S' });
+        }
+    }
+}
