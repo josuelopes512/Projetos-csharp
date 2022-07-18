33b96201511c1f523675f4d4c5bbee21b89fd6c1
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-18 09:21:04 -0300

    add atividade Identificando o Chá

---
+++ 2006/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count=0, valor = int.Parse(Console.ReadLine());
+            int[] array = Array.ConvertAll(Console.ReadLine().TrimEnd().Split(' '), int.Parse);
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == valor) count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
