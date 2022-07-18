769ba031ff03924df85532e3ffda0d68917cc5f9
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-18 09:57:24 -0300

    add atividade Busca na Internet

---
+++ 2413/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2413
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+            Console.WriteLine((valor*2)*2);
+           
+        }
+    }
+}
