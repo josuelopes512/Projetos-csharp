f4595ab64b9f96b31b345be85edd96cc97ab75d5
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-18 10:21:18 -0300

    add atividade Sequência Lógica

---
+++ 1144/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1144
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+            int a=0, b=0, c=0, d=0;
+
+            for (int i = 1; i <= valor; i++)
+            {
+                a = i*i;
+                b = i*i*i;
+                Console.WriteLine($"{i} {a} {b}");
+                c = a+1;
+                d = b+1;
+                Console.WriteLine($"{i} {c} {d}");
+            }
+        }
+    }
+}
