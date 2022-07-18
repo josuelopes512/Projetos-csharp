8277c6945d7ecea99b7a313b4e5d897625bd2ed5
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-18 10:02:10 -0300

    add atividade Quadrado e ao Cubo

---
+++ 1143/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1143
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= valor; i++)
+            {
+                Console.WriteLine($"{i} {i*i} {i*i*i}");
+            }
+        }
+    }
+}
