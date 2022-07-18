d097138cc0c6442c44abde32472f1fc4c4228dfe
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-18 09:29:45 -0300

    add atividade Fatorial Simples

---
+++ 1153/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1153
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long m=1;
+            int valor = int.Parse(Console.ReadLine());
+
+            for (int i = valor; i >=1; i--)
+            {
+                m *= i;
+            }
+            Console.WriteLine(m);
+        }
+    }
+}
