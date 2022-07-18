237da6c64efe419e612d4021f687e12b3ee30af0
Josue Lopes <josue.s.lopes.512@gmail.com>
2022-07-18 10:06:29 -0300

    add atividade Divisores I

---
+++ 1157/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1157
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= valor; i++)
+            {
+                if(valor % i == 0) Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
